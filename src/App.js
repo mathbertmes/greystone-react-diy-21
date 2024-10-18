5a3c7847e17c258239f5872226e2c3ed9c8e4fb0
Matheus Bertemes <mathbertemes@gmail.com>
2024-10-17 19:49:19 -0700

    app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
+
+import { Box, Button, CircularProgress, Skeleton, Slider, TextField } from '@mui/material';
 import './App.css';
+import SimpleSnackbar from './components/snackbar';
+import AccordionUsage from './components/accordion';
+import ButtonAppBar from './components/app-bar';
+import TemporaryDrawer from './components/drawer';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>
+        <Slider defaultValue={50} aria-label="Default" valueLabelDisplay="auto" />
+        <Button variant="contained" color="error">
+          DELETE
+        </Button>
+        <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+        <TextField id="standard-basic" label="Standard" variant="standard" />
+      </div>
+
+      <div>
+      <CircularProgress />
+      </div>
+      <div>
+      <Skeleton variant="rectangular" width={210} height={118} />
+      <Box sx={{ pt: 0.5 }}>
+              <Skeleton />
+              <Skeleton width="60%" />
+            </Box>
+      </div>
+      <div>
+        <SimpleSnackbar />
+      </div>
+      <div>
+        <AccordionUsage />
+      </div>
+      <div>
+        <ButtonAppBar />
+      </div>
+      <div>
+        <TemporaryDrawer />
+      </div>
     </div>
   );
 }
